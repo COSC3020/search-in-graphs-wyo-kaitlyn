570aeaa5dad65ccd8aaf7069d11687ae0c6a833a
wyo-kaitlyn <wyokaitlyn@gmail.com>
2023-12-06 16:34:31 -0700

    updated test code

--- code.test.js
+++ code.test.js
@@ -4,10 +4,10 @@ eval(fs.readFileSync('code.js')+'');
 
 // took this test code from JustinQuanCC's github
 
-assert(JSON.stringify(depthFirstSearch([], 0, 1)), JSON.stringify([]));
+assert(JSON.stringify(depthFirstSearch([], 0, 1)) == JSON.stringify([]));
 
-assert(JSON.stringify(depthFirstSearch([[1],[0,3],[0,1],[0,1,2,3]], 0, 2)), JSON.stringify([0,1,3,2]));
+assert(JSON.stringify(depthFirstSearch([[1],[0,3],[0,1],[0,1,2,3]], 0, 2)) == JSON.stringify([0,1,3,2]));
 
-assert(JSON.stringify(depthFirstSearch([[1],[0,3],[0,1],[0,1,3]], 0, 2)), JSON.stringify([]));
+assert(JSON.stringify(depthFirstSearch([[1],[0,3],[0,1],[0,1,3]], 0, 2)) == JSON.stringify([]));
 
-assert(JSON.stringify(depthFirstSearch([[0,1],[2],[1,3],[2,4],[0]], 1, 0)), JSON.stringify([1,2,3,4,0]));
+assert(JSON.stringify(depthFirstSearch([[0,1],[2],[1,3],[2,4],[0]], 1, 0)) == JSON.stringify([1,2,3,4,0]));
