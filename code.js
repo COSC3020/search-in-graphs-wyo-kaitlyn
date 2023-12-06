20735bdf2134fbd416756e28434ae1e390c8f5f2
wyo-kaitlyn <wyokaitlyn@gmail.com>
2023-12-05 21:45:24 -0700

    dfs

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
+let visitedNodes = [];
+
 function depthFirstSearch(graph, startNode, targetNode) {
+    if (startNode == targetNode) { return visitedNodes; }
+    else if (graph.length < 1) { return []; }
+    else {
+        if (!visitedNodes[startNode]) {
+            visitedNodes.push(Number(startNode));
+            // have to ensure they are the same data type else the 
+            // check to see if an item is in an array will not 
+            // recognize it is already in there and add it again
+            console.log(visitedNodes)
+        }
+        for(node of graph[startNode]) {
+            if (!visitedNodes[Number(node)]) {
+                depthFirstSearch(graph, node, targetNode);
+            }
+        }
+    }
     return [];
 }
